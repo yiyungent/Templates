b8517f5a8ad54bd9045e8ed03a25234f9fb48964
yiyun <yiyungent@gmail.com>
2019-05-07 22:28:38 +0800

    first copy

---
+++ README.md
@@ -0,0 +1,13 @@
+<p align="center">
+
+</p>
+<h1 align="center">Templates</h1>
+
+> :cake: ASP.NET MVC5 多主题模板解决方案
+
+[![repo size](https://img.shields.io/github/repo-size/yiyungent/Templates.svg?style=flat)]()
+[![LICENSE](https://img.shields.io/github/license/yiyungent/Templates.svg?style=flat)](https://mit-license.org/)
+[![nuget](https://img.shields.io/nuget/v/Templates.svg?style=flat)](https://www.nuget.org/packages/Templates/)
+[![downloads](https://img.shields.io/nuget/dt/Templates.svg?style=flat)](https://www.nuget.org/packages/Templates/)
+
+
---
+++ src/Templates/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Templates")]
+[assembly: AssemblyDescription("ASP.NET MVC5 多主题模板解决方案")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("yiyun")]
+[assembly: AssemblyProduct("Templates")]
+[assembly: AssemblyCopyright("Copyright (c) 2019 yiyun")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("619d5267-4659-4ff5-a131-53be0ab85001")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
